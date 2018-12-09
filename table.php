6c8ffabd7b34f39820243a23a8322861b9148982
nasubi613 <aoi.saxophone13@gmail.com>
2018-12-09 14:41:49 +0900

    test tochuu

--- table.php
+++ table.php
@@ -5,19 +5,24 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="reset.css">
 	<title>table</title>
 </head>
 <body>
 
 <?php
 $arr = array();
-$fp = fopen('data.txt', 'r');
-while($line = fgets($fp)){
+$fpr = fopen('data.txt', 'r');
+while($line = fgets($fpr)){
 	$arr = $line;
 }
-fclose($fp);
+fclose($fpr);
+foreach($arr as $val){
+	echo $val;
+}
 ?>
+
+<form action="table.php" method="post">
 <table>
 	<tr>
 		<th></th>
@@ -25,7 +30,55 @@ fclose($fp);
 		<th>2</th>
 		<th>3</th>
 	</tr>
+	<tr>
+		<th>Aさん</th>
+		<td><input type="text" name="input01" value="<?=$arr[0]?>"></td>
+		<td><input type="text" name="input02" value="<?=$arr[1]?>"></td>
+		<td><input type="text" name="input03" value="<?=$arr[2]?>"></td>
+	</tr>
+	<tr>
+		<th>Bさん</th>
+		<td><input type="text" name="input04" value="<?=$arr[3]?>"></td>
+		<td><input type="text" name="input05" value="<?=$arr[4]?>"></td>
+		<td><input type="text" name="input06" value="<?=$arr[5]?>"></td>
+	</tr>
+	<tr>
+		<th>Cさん</th>
+		<td><input type="text" name="input07" value="<?=$arr[6]?>"></td>
+		<td><input type="text" name="input08" value="<?=$arr[7]?>"></td>
+		<td><input type="text" name="input09" value="<?=$arr[8]?>"></td>
+	</tr>
 </table>
+</form>
+
+<?php
+$fpw = fopen('data.txt', 'w');
+for($i = 1; $i < 10; $i++){
+	$selecter = 'input0' + $i;
+	
+}
+$_POST[]
+?>
 
 </body>
+
+<style>
+* {
+	box-sizing: border-box;
+}
+table {
+	background: skyblue;
+	border-collapse: collapse;
+	margin: 50px auto;
+}
+table tr th,
+table tr td {
+	width: 150px;
+	border: 1px solid #666;
+	padding: 5px;
+}
+input {
+	width: 100%;
+}
+</style>
 </html>
